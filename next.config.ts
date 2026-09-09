6afc4ac1cd24d3f9f72dadf3663a2ea68f5eda17
itsmeaj27 <ajays.sharma27@gmail.com>
2026-09-10 00:42:01 +0530

    chore(config): configure codingclubcuh.online and standard remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,22 +5,38 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      new URL("https://randomuser.me/api/portraits/**"),
-      new URL("https://alt.tailus.io/images/team/**"),
-      new URL("https://res.cloudinary.com/**"),
-      new URL("https://assets.aceternity.com/templates/**"),
-      new URL("https://assets.aceternity.com/**"),
-      new URL('http://localhost:3000/api/media/file/**'),
-      new URL('http://localhost:3001/api/media/file/**'),
-      new URL('https://codingclubcuh.online/api/media/file/**'),
-      new URL('https://www.codingclubcuh.online/api/media/file/**'),
-      new URL('https://cuhcodingclub.netlify.app/api/media/file/**'),
-      new URL('http://localhost:3000/og-image/**'),
-      new URL('http://localhost:3001/og-image/**'),
-      new URL('https://codingclubcuh.online/og-image/**'),
-      new URL('https://www.codingclubcuh.online/og-image/**'),
-      new URL('https://cuhcodingclub.netlify.app/og-image/**'),
-
+      {
+        protocol: 'https',
+        hostname: 'codingclubcuh.online',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.codingclubcuh.online',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cuhcodingclub.netlify.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'alt.tailus.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.aceternity.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
     ],
   },
 };
